333f2c293344686a6307494a76e60bfe3c3090af
ryanearwaker <ryanearwaker@localhost>
2020-07-27 23:17:41 +1200

    mhgnbv

--- main.c
+++ main.c
@@ -60,16 +60,6 @@ int main(void)
         while(1);               // Oh no! Must not have had enough memory to create the task.
     }
 
-    //
-    // Create the switch task.
-    //
-    if(SwitchTaskInit() != 0)
-    {
-
-        while(1)
-        {
-        }
-    }
 
 
     vTaskStartScheduler();      // Start FreeRTOS!!
