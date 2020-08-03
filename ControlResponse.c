a712f3390681794f3c539f7acfd9b5d8f78c224c
ryanearwaker <ryanearwaker@ryans-mbp>
2020-08-03 12:29:36 +1200

    Added more to the control, pre pseudo code atm

--- ControlResponse.c
+++ ControlResponse.c
@@ -14,12 +14,15 @@ int int_error;
 
 int control;
 
+#define OUT_MAX; //need to find out max and min values
+#define OUT_MIN;
+
 
 void flightController(int target, int input, int output) {
 
     int time_step = SysCtlClockGet() / 1000;   //get time step //xTaskGetTickCount()/80000 //gives 1ms?
 
-    double error = target - input; //calculate the error
+    double error = target - input; //calculate the error, critical section so disable ints etc
 
 
     control = kp*error + kd*(prev_error - error)/time_step + int_error;
@@ -28,6 +31,12 @@ void flightController(int target, int input, int output) {
 
     int_error+= (ki * error); //sum for int control
 
+    if(control > OUT_MAX) {control = OUT_MAX;}
+
+    if(control < OUT_MIN) {control = OUT_MIN;}
+
     output = control;
+
+    vTaskDelay(time_step); for 1ms
 }
 */
