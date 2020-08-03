63f3eafd7d568212926db492c29fcd3be188973c
ryanearwaker <ryanearwaker@ryans-mbp>
2020-08-03 13:09:39 +1200

    changed nothing

--- ControlResponse.c
+++ ControlResponse.c
@@ -17,13 +17,11 @@ int control;
 #define OUT_MAX; //need to find out max and min values
 #define OUT_MIN;
 
-
 void flightController(int target, int input, int output) {
 
     int time_step = SysCtlClockGet() / 1000;   //get time step //xTaskGetTickCount()/80000 //gives 1ms?
 
     double error = target - input; //calculate the error, critical section so disable ints etc
-
 
     control = kp*error + kd*(prev_error - error)/time_step + int_error;
 
