740fff3d8fdf70684084e4636cd547ac8d6ba9bd
mattblake <mjb409@uclive.ac.nz>
2020-08-06 23:45:21 +1200

    fuck

--- main.c
+++ main.c
@@ -297,7 +297,7 @@ void
 init(void)
 {
     initClk();
-    initLED();
+    //initLED();
     OLEDInitialise();
     initBtns();
     initialiseUSB_UART();
@@ -314,7 +314,7 @@ init(void)
 void
 createTasks(void)
 {
-    xTaskCreate(BlinkLED,       "LED Task",     LED_STACK_DEPTH,        NULL,       LED_TASK_PRIORITY,      NULL);
+    //xTaskCreate(BlinkLED,       "LED Task",     LED_STACK_DEPTH,        NULL,       LED_TASK_PRIORITY,      NULL);
     xTaskCreate(OLEDDisplay,    "OLED Task",    OLED_STACK_DEPTH,       NULL,       OLED_TASK_PRIORITY,     NULL);
     xTaskCreate(ButtonsCheck,   "Btn Poll",     BTN_STACK_DEPTH,        NULL,       BTN_TASK_PRIORITY,      NULL);
     xTaskCreate(Cringe_ADC,     "ADC Handler",  ADC_STACK_DEPTH,        NULL,       ADC_TASK_PRIORITY,      NULL);
