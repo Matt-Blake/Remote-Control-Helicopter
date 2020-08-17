40c6efddc96ba7975412fe8c0be5816b94649bbc
ryanearwaker <ryanearwaker@ryans-mbp>
2020-08-17 23:01:11 +1200

    Fixed

--- pwm.c
+++ pwm.c
@@ -144,15 +144,7 @@ initPWM (void)
 void
 setRotorPWM (uint32_t ui32Duty, bool SET_MAIN)
 {
-    //char string[20];
     // Calculate the PWM period corresponding to the freq.
-<<<<<<< HEAD
-    //usnprintf(string, sizeof(string), "ACTUALPWM\n\r", ui32Duty);
-    //UARTSend(string);
-=======
-    usnprintf(string, sizeof(string), "ACTUALPWM %d\n\r", ui32Duty);
-    UARTSend(string);
->>>>>>> branch 'master' of ssh://git@eng-git.canterbury.ac.nz/ence464-2020/group2.git
     uint32_t ui32Period = SysCtlClockGet() / PWM_DIVIDER / PWM_START_RATE_HZ;
 
     if (ui32Duty > 98){
