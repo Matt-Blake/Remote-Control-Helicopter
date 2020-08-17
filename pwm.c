7cb553d42144d8e4edbf0c3dabc3dc624cfd2bed
ryanearwaker <ryanearwaker@ryans-mbp>
2020-08-17 22:59:37 +1200

    Fixed FindRef

--- pwm.c
+++ pwm.c
@@ -144,10 +144,10 @@ initPWM (void)
 void
 setRotorPWM (uint32_t ui32Duty, bool SET_MAIN)
 {
-    char string[20];
+    //char string[20];
     // Calculate the PWM period corresponding to the freq.
-    usnprintf(string, sizeof(string), "ACTUALPWM\n\r", ui32Duty);
-    UARTSend(string);
+    //usnprintf(string, sizeof(string), "ACTUALPWM\n\r", ui32Duty);
+    //UARTSend(string);
     uint32_t ui32Period = SysCtlClockGet() / PWM_DIVIDER / PWM_START_RATE_HZ;
 
     if (ui32Duty > 98){
