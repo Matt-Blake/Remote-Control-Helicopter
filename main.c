3b4b55171e7170b95f9e382e7dd8d0422bb7c41f
grayson-uc <grayson.mynott@gmail.com>
2020-08-18 20:54:56 +1200

    Removed unnecessary includes

--- main.c
+++ main.c
@@ -22,14 +22,9 @@
 // Basic/Board includes
 #include <stdbool.h>
 #include <stdint.h>
-#include "inc/hw_memmap.h"
-#include "inc/hw_types.h"
-#include "driverlib/gpio.h"
 #include "driverlib/interrupt.h"
-#include "driverlib/pin_map.h"
-#include "driverlib/sysctl.h"
 #include "driverlib/systick.h"
-#include "utils/ustdlib.h"
+//#include "utils/ustdlib.h"
 #include "OrbitOLED/OrbitOLEDInterface.h"
 
 // FreeRTOS includes
