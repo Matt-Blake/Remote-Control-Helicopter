87d07ba1e456f52e087c0645671b62a7d19cef09
grayson-uc <grayson.mynott@gmail.com>
2020-08-19 01:35:35 +1200

    Cleaned

--- buttons.h
+++ buttons.h
@@ -40,34 +40,6 @@ enum btnNames   {UP = 0, DOWN, LEFT, RIGHT, NUM_BTNS};
 enum btnStates  {RELEASED = 0, PUSHED, NO_CHANGE};
 
 
-#define U_BTN_PERIPH        SYSCTL_PERIPH_GPIOE         // Up Peripheral
-#define U_BTN_PORT_BASE     GPIO_PORTE_BASE             // Up Port Base
-#define U_BTN_PIN           GPIO_PIN_0                  // Up Pin
-#define U_BTN_NORMAL        false                       // Up Inactive State (Active HIGH)
-#define D_BTN_PERIPH        SYSCTL_PERIPH_GPIOD         // Down Peripheral
-#define D_BTN_PORT_BASE     GPIO_PORTD_BASE             // Down Port Base
-#define D_BTN_PIN           GPIO_PIN_2                  // Down Pin
-#define D_BTN_NORMAL        false                       // Down Inactive State (Active HIGH)
-#define L_BTN_PERIPH        SYSCTL_PERIPH_GPIOF         // Left Peripheral
-#define L_BTN_PORT_BASE     GPIO_PORTF_BASE             // Left Port Base
-#define L_BTN_PIN           GPIO_PIN_4                  // Left Pin
-#define L_BTN_NORMAL        true                        // Left Inactive State (Active LOW)
-#define R_BTN_PERIPH        SYSCTL_PERIPH_GPIOF         // Right Peripheral
-#define R_BTN_PORT_BASE     GPIO_PORTF_BASE             // Right Port Base
-#define R_BTN_PIN           GPIO_PIN_0                  // Right Pin
-#define R_BTN_NORMAL        true                        // Right Inactive State (Active LOW)
-#define NUM_BTN_POLLS       3                           // Number Of Times To Poll The Buttons (For Debouncing)
-#define SW_PERIPH           SYSCTL_PERIPH_GPIOA         // Switch Peripheral
-#define SW_PORT_BASE        GPIO_PORTA_BASE             // Switch Port Base
-#define L_SW_PIN            GPIO_PIN_6                  // Left Switch Pin
-#define R_SW_PIN            GPIO_PIN_7                  // Right Switch Pin
-#define ALT_CHANGE          10                          // The altitude change on button press (percentage)
-#define MAX_ALT             100                         // The maximum altitude (percentage)
-#define MIN_ALT             0                           // The minimum altitude (percentage)
-#define YAW_CHANGE          15                          // The yaw change on button press (degrees)
-#define MAX_YAW             164                         // The maximum yaw (degrees) before increment
-#define MIN_YAW             -165                        // The minimum yaw (degrees) before increment
-#define DEGREES_CIRCLE      360                         // The number of degrees in a circle
 #define BTN_TASK_PRIORITY   5                           // Button polling task priority
 #define SWI_TASK_PRIORITY   5                           // Switch polling task priority
 #define BTN_STACK_DEPTH     64
