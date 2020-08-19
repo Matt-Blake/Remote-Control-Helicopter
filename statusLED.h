6e7080c83cf759e86b500e6a2d51d8e88d0f26e7
mattblake <mjb409@uclive.ac.nz>
2020-08-19 18:09:30 +1200

    Updated blinky file descriptions

--- statusLED.h
+++ statusLED.h
@@ -1,9 +1,17 @@
-/*
+/* ****************************************************************
  * statusLED.h
  *
- *  Created on: 19/08/2020
- *      Author: mattblake
- */
+ * Header file for the statusLED module
+ * Flash the blue LED on the Tiva board to indicate that the program
+ * is running.
+ *
+ * ENCE464 Assignment 1 Group 2
+ * Creators: Grayson Mynott      56353855
+ *           Ryan Earwaker       12832870
+ *           Matt Blake          58979250
+ * Last modified: 19/08/2020
+ *
+ * ***************************************************************/
 
 #ifndef STATUSLED_H_
 #define STATUSLED_H_
