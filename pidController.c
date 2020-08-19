b52f2f4e188babc3e1c0827d5dbecd9086dd8538
ryanearwaker <ryanearwaker@ryans-mbp>
2020-08-19 13:59:35 +1200

    gains change

--- pidController.c
+++ pidController.c
@@ -27,7 +27,7 @@
 
 #define YAW_KP              30         // Yaw proportional gain
 #define YAW_KI              5          // Yaw integral gain
-#define YAW_KD              15          // Yaw derivative gain
+#define YAW_KD              10          // Yaw derivative gain
 
 #define CONTROL_DIVISOR     100         // Divisor used to achieve certain gains without the use of floating point numbers
 
